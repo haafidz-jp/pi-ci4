9785155e300cc4f562bd38cf1fc283ab5b4f09c7
Haafidz Nurul Salim <haafidz123@gmail.com>
2021-06-08 23:05:57 +0700

    perubahan baseURL

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'https://haafidzci4.herokuapp.com/';
+	public $baseURL = 'http://localhost:8080/';
 
 	/*
 	|--------------------------------------------------------------------------
