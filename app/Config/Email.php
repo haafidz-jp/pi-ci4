ec9e03a063311c3ee4b7fbac63b74710f93fe6e6
Haafidz Nurul Salim <haafidz123@gmail.com>
2021-07-02 13:29:16 +0700

    setting email

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -9,12 +9,12 @@ class Email extends BaseConfig
 	/**
 	 * @var string
 	 */
-	public $fromEmail;
+	public $fromEmail = 'cs@haafidz.xyz';
 
 	/**
 	 * @var string
 	 */
-	public $fromName;
+	public $fromName = 'cs';
 
 	/**
 	 * @var string
